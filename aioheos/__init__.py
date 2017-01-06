05a7f9b27392fc325933f5127efe334d163200af
Andreas Rydbrink <andreas.rydbrink@gmail.com>
2017-01-06 09:39:17 +0100

    missed AioHeosException

--- aioheos/__init__.py
+++ aioheos/__init__.py
@@ -1,5 +1,5 @@
 from .version import __version__
-from .aioheos import AioHeos
+from .aioheos import AioHeos, AioHeosException
 from .aioheosupnp import AioHeosUpnp
 
 # __all__ = ['AioHeos', 'AioHeosUpnp']
