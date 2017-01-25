3b39b2efa97de7c3d96cdd3dd08a8a12be91f8bc
Andreas Rydbrink <andreas.rydbrink@gmail.com>
2017-01-25 22:10:43 +0100

    fix v0.0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='aioheos',
-      version='0.0.1',
+      version='0.0.3',
       description='Denon Heos',
       url='http://github.com/easink/aioheos',
       author='Andreas Rydbrink',
